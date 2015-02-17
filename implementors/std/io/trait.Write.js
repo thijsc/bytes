fb3ca151a1e30d54d734cfd3644d66d227611d3c
Carl Lerche <me@carllerche.com>
2015-02-17 04:31:16 +0000

    rebuild pages at e39ba25

---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bytes'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active deveopment and APIs are subject to tweaks fairly regularly'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.MutByteBuf.html' title='bytes::MutByteBuf'>MutByteBuf</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
