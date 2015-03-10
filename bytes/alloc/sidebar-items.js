c0c957260da63fef3cc91ed5c23735ef148daa3e
Carl Lerche <me@carllerche.com>
2015-03-10 05:22:26 +0000

    rebuild pages at 113bff2

---
+++ bytes/alloc/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["Allocator","Allocates memory to be used by Bufs or Bytes. Allows allocating memory using alternate stratgies than the default Rust heap allocator. Also does not require that allocations are continuous in memory."]]});
--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bytes'] = ["<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='bytes/struct.Bytes.html' title='bytes::Bytes'>Bytes</a>",];
+implementors['bytes'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='bytes/struct.Bytes.html' title='bytes::Bytes'>Bytes</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
