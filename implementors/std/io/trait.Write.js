782978f4bad1f9e2ed0592dff1cd5549aab690f0
Carl Lerche <me@carllerche.com>
2015-03-19 16:05:17 +0000

    rebuild pages at 3f9b7f6

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bytes'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active development and APIs are subject to tweaks fairly regularly'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.MutByteBuf.html' title='bytes::MutByteBuf'>MutByteBuf</a>",];
+implementors['bytes'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.MutByteBuf.html' title='bytes::MutByteBuf'>MutByteBuf</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
